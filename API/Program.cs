93b0f449cc130a2ce5dcdf9cb0d51d10de824c3d
sakabiruddin <145967210+sakabiruddin@users.noreply.github.com>
2024-03-30 16:49:01 +0600

    Version 2.0

--- API/Program.cs
+++ API/Program.cs
@@ -10,16 +10,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
- 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
