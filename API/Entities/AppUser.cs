df880cc0f1f548b48137804595d4e6b1971e1e55
pro777oy <uddin.saadkabir@gmail.com>
2024-09-23 18:07:30 +0600

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entities;
 
 public class AppUser
 {
-    public int Id { get; set; }
+    public int Id { get; init; }
+    
+    [MaxLength(100)]
     public required string UserName { get; set; }
+    
+    public required byte[] PasswordHash { get; set; }
+    
+    public required byte[] PasswordSalt { get; set; }
 }
