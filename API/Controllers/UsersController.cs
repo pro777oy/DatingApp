1a55adeebfc6492ba208a665191ebfb192cefe96
Saad Kabir Uddin <102184777+pro777oy@users.noreply.github.com>
2025-01-18 15:25:22 +0600

    script002

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,22 +1,22 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController(DataContext context) : ControllerBase
+public class UsersController(DataContext context) : BaseApiController
 {
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>>  GetUsers()
     {
         var users = await context.Users.ToListAsync();
         return users;
     }
-
+    [Authorize]
     [HttpGet("{id:int}")] // /api/users/2
     public async Task<ActionResult<AppUser>> GetUserById(int id)
     {
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,36 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+public class TokenService (IConfiguration config) : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        var tokenKey = config["TokenKey"] ?? throw new Exception("TokenKey is empty in app settings");
+        if(tokenKey.Length < 64) throw new Exception("Invalid token key length");
+        
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Name, user.UserName),
+        };
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        { 
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = credentials
+        };
+        
+        var tokenHandler= new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        
+        return tokenHandler.WriteToken(token);
+    }
+}
