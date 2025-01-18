1aff3bd11a43af4bc7e6c50ea6e5597d340e7f7e
Saad Kabir Uddin <102184777+pro777oy@users.noreply.github.com>
2025-01-18 15:25:22 +0600

    script002

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,5 +9,7 @@ public class AppUser
     [StringLength(100)]
     public required string UserName { get; set; }
     
+    public required byte[] PasswordHash { get; set; }
     
+    public required byte[] PasswordSalt { get; set; }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,13 +12,39 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>
+
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        var tokenKey= builder.Configuration["TokenKey"]?? throw new Exception("TokenKey is missing");
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey((Encoding.UTF8.GetBytes(tokenKey))),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+
+        };
+    });
 var app = builder.Build();
 
 
 app.UseHttpsRedirection();
+
+app.UseCors(x =>
+    x.AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithOrigins("http://localhost:4200","https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
